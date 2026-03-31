c12b595b608b2191ab2e766da15faf09b99372ac
Gaël Raj <gaelrajaomiandrisoa@gmail.com>
2026-03-31 10:40:28 +0300

    refactor: remove JdbcTemplate and use dataSource

--- src/main/java/com/restaurant/td5springboot/repository/IngredientRepository.java
+++ src/main/java/com/restaurant/td5springboot/repository/IngredientRepository.java
@@ -1,22 +1,22 @@
 package com.restaurant.td5springboot.repository;
 
 import com.restaurant.td5springboot.entity.*;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataAccessException;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
-import java.sql.Array;
-import java.sql.Connection;
-import java.sql.Timestamp;
+import javax.sql.DataSource;
+import java.sql.*;
 import java.time.Instant;
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
 public class IngredientRepository {
 
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
+    private final DataSource dataSource;
+
+    public IngredientRepository(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     public List<Ingredient> findAll() {
         String sql = """
@@ -25,16 +25,26 @@ public class IngredientRepository {
                 ORDER BY id
                 """;
 
-        return jdbcTemplate.query(sql, (rs, rowNum) -> {
-            Ingredient ingredient = new Ingredient();
+        List<Ingredient> ingredients = new ArrayList<>();
 
-            ingredient.setId(rs.getInt("id"));
-            ingredient.setName(rs.getString("name"));
-            ingredient.setPrice(rs.getDouble("price"));
-            ingredient.setCategory(CategoryEnum.valueOf(rs.getString("category")));
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement ps = connection.prepareStatement(sql);
+             ResultSet rs = ps.executeQuery()) {
 
-            return ingredient;
-        });
+            while (rs.next()) {
+                Ingredient ingredient = new Ingredient();
+                ingredient.setId(rs.getInt("id"));
+                ingredient.setName(rs.getString("name"));
+                ingredient.setPrice(rs.getDouble("price"));
+                ingredient.setCategory(CategoryEnum.valueOf(rs.getString("category")));
+                ingredients.add(ingredient);
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return ingredients;
     }
 
     public Ingredient findIngredientById(int idIngredient) {
@@ -43,62 +53,90 @@ public class IngredientRepository {
                 FROM Ingredient
                 WHERE id = ?
                 """;
-        try {
-            return jdbcTemplate.queryForObject(sql, (rs, rowNum) -> {
-                Ingredient ingredient = new Ingredient();
 
-                ingredient.setId(rs.getInt("id"));
-                ingredient.setName(rs.getString("name"));
-                ingredient.setPrice(rs.getDouble("price"));
-                ingredient.setCategory(CategoryEnum.valueOf(rs.getString("category")));
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement ps = connection.prepareStatement(sql)) {
 
-                return ingredient;
-            }, idIngredient );
-        } catch (DataAccessException e) {
-            throw new RuntimeException("Ingredient.id=" + idIngredient + " is not found");
+            ps.setInt(1, idIngredient);
+
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    Ingredient ingredient = new Ingredient();
+                    ingredient.setId(rs.getInt("id"));
+                    ingredient.setName(rs.getString("name"));
+                    ingredient.setPrice(rs.getDouble("price"));
+                    ingredient.setCategory(CategoryEnum.valueOf(rs.getString("category")));
+                    return ingredient;
+                } else {
+                    throw new RuntimeException("Ingredient.id=" + idIngredient + " is not found");
+                }
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
     }
 
     public StockValue findStockValueAt(int idIngredient, Instant at) {
         String sql = """
-                select unit,
-                sum(
-                    case 
-                        when type = 'IN' then quantity
-                        else -quantity
-                    end 
-                ) as current_stock
-                from stockmovement
-                where id_ingredient = ?
-                    and creation_datetime <= ?
-                group by unit
+                SELECT
+                    SUM(CASE WHEN type = 'IN' THEN quantity ELSE -quantity END) as current_stock
+                FROM stockmovement
+                WHERE id_ingredient = ?
+                    AND creation_datetime <= ?
                 """;
 
-        return jdbcTemplate.queryForObject(sql,(rs, rowNum) -> {
-            StockValue stockValue = new StockValue();
-            stockValue.setQuantity(rs.getDouble("current_stock"));
-            stockValue.setUnit(Unit.valueOf(rs.getString("unit")));
-            return stockValue;
-        }, idIngredient, Timestamp.from(at));
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement ps = connection.prepareStatement(sql)) {
+
+            ps.setInt(1, idIngredient);
+            ps.setTimestamp(2, Timestamp.from(at));
+
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    StockValue stockValue = new StockValue();
+                    stockValue.setQuantity(rs.getDouble("current_stock"));
+                    return stockValue;
+                } else {
+                    throw new RuntimeException("No stock found for Ingredient.id=" + idIngredient);
+                }
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public List<Ingredient> findAllByIds(List<Integer> ids) {
         String sql = """
-            SELECT id, name, price, category
-            FROM Ingredient
-            WHERE id = ANY(?)
-            """;
+                SELECT id, name, price, category
+                FROM Ingredient
+                WHERE id = ANY(?)
+                """;
 
-        Array array = jdbcTemplate.execute((Connection con) ->
-                con.createArrayOf("integer",
-                        ids.toArray()));
+        List<Ingredient> ingredients = new ArrayList<>();
 
-        return jdbcTemplate.query(sql, (rs, rowNum) -> new Ingredient(
-                rs.getInt("id"),
-                rs.getString("name"),
-                rs.getDouble("price"),
-                CategoryEnum.valueOf(rs.getString("category"))
-        ), array);
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement ps = connection.prepareStatement(sql)) {
+
+            Array array = connection.createArrayOf("integer", ids.toArray());
+            ps.setArray(1, array);
+
+            try (ResultSet rs = ps.executeQuery()) {
+                while (rs.next()) {
+                    Ingredient ingredient = new Ingredient();
+                    ingredient.setId(rs.getInt("id"));
+                    ingredient.setName(rs.getString("name"));
+                    ingredient.setPrice(rs.getDouble("price"));
+                    ingredient.setCategory(CategoryEnum.valueOf(rs.getString("category")));
+                    ingredients.add(ingredient);
+                }
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return ingredients;
     }
-
 }
